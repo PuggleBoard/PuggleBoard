03c9c829680d834736b088328d6da8b1fa5c14ec
jonnew <jnewman6@gatech.edu>
2014-06-05 22:00:06 -0400

    Finalized BOM. This is r3.0

--- board/bom/bom_parse.m
+++ board/bom/bom_parse.m
@@ -1,14 +1,18 @@
 % Parse the BOM CSV into something I can upload on DK
 
-fid = './PuggleBoard.csv';
+fid = './PuggleBoard_DK.csv';
 f = fopen(fid);
-x = sort(textread(fid,'%s','delimiter',','));
-
+X = textread(fid,'%s','delimiter',',')
+DK = X(1:2:end);
+TYPE = X(2:2:end);
+[x, i] = sort(DK);
+t = TYPE(i);
 
 [y,  n] = unique(x);
+t = t(n);
 num = diff([n; numel(x)+1]);
 num_cell = num2cell(num);
-list = [y num_cell];
+list = [y num_cell t];
 
 xlswrite('./PuggleBoard_DK',list)
 
